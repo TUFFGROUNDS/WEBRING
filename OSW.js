253b9c8dff170680ae61a9e7b7bc0c621783c14d
TUFFGROUNDS <tuffgrounds@proton.me>
2025-10-01 14:49:14 +0000

    Update OSW.js

--- OSW.js
+++ OSW.js
@@ -1,13 +1,17 @@
 // SERVER CONFIG
-const RINGSCRIPT_url = https://example.com/ring.js;
+const RING_SCRIPT_url = https://example.com/ring.js;
 
 
 // RING CONFIG
-const RING_name = OSW-Webring;
+const RING_name = OSW;
 
 const RING_homepage = https://www.ringhomepage.com;
+const RING_admin = Name of the Ring Admin;
+const RING_admin_homepage = https://www.admins-homepage.com;
 
 
+const show_RING_admin = true; // whether or not the name of the admin should get shown on the ring widget
+const show_RING_admin_homepage = true; // whether or not the the admins homepage should be linked to (only if show_RING_admin = true)
 
 
 var base_url = window.location.origin;
