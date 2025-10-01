53ed9c8dadfc34e68681755469072fe99bf148b8
TUFFGROUNDS <tuffgrounds@proton.me>
2025-10-01 14:44:47 +0000

    Create OSW.js

---
+++ OSW.js
@@ -0,0 +1,17 @@
+// SERVER CONFIG
+const RINGSCRIPT_url = https://example.com/ring.js;
+
+
+// RING CONFIG
+const RING_name = OSW-Webring;
+
+const RING_homepage = https://www.ringhomepage.com;
+
+
+
+
+var base_url = window.location.origin;
+
+var host = window.location.host;
+
+var pathArray = window.location.pathname.split( '/' );
