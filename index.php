8aea17e398656a1ef98e1d56627674e9161d9de8
TUFFGROUNDS <tuffgrounds@proton.me>
2025-10-05 22:28:57 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+    <link href="/style.css" rel="stylesheet" type="text/css" media="all">
+  </head>
+  <body>
+  
+<center>
+
+<h2>Webring Widget</h2>
+<div id="ring-ringid"></div>
+
+<p></p>
+<h2>These are the members of the Webring</h2>
+<div id="ring-ringmembers"></div>
+
+</center>
+
+<script src="/tuffring.js"></script>
+  </body>
+</html>
